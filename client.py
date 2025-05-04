29cc7a5c97778885f9bd248f532077844165dcf3
ElmahdiAmmari <elmahdi.583.92@email.com>
2025-05-04 12:58:24 +0100

    create multiple client up t 10

--- client.py
+++ client.py
@@ -1,7 +1,30 @@
 import socket
+import threading
+
+def receive_messages(sock):
+    while True:
+        try:
+            msg = sock.recv(1024).decode()
+            if msg:
+                print("\n" + msg)
+        except:
+            print("[!] Connection closed.")
+            break
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 9999))
 
-client.send("hello from client".encode())
+# Receiving prompt for username
 print(client.recv(1024).decode())
+username = input()
+client.send(username.encode())
+
+# Start thread to receive messages
+threading.Thread(target=receive_messages, args=(client,), daemon=True).start()
+
+# Send messages
+while True:
+    msg = input("To send (format TO:username:message): ")
+    if msg.lower() == "quit":
+        break
+    client.send(msg.encode())
